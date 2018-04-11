0f5db51c09d72231ecddb5144dbdb3beec441212
Your Name <supuni.nimeshika@gmail.com>
2018-04-11 20:26:45 +0530

    Add Service Methods

--- app/src/main/java/com/example/supuni/mybusapplication/GPSTracker.java
+++ app/src/main/java/com/example/supuni/mybusapplication/GPSTracker.java
@@ -1,8 +1,42 @@
 package com.example.supuni.mybusapplication;
 
+import android.app.Service;
+import android.bluetooth.BluetoothClass;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
 /**
  * Created by supuni on 4/11/18.
  */
 
-public class GPSTracker {
+public class GPSTracker extends Service implements LocationListener{
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
 }
