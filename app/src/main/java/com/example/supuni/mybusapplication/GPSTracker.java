fb4f9759affcafecf5e6dae5e958914051195cb1
Your Name <supuni.nimeshika@gmail.com>
2018-04-11 22:10:16 +0530

    Add Get Location Method

--- app/src/main/java/com/example/supuni/mybusapplication/GPSTracker.java
+++ app/src/main/java/com/example/supuni/mybusapplication/GPSTracker.java
@@ -1,11 +1,13 @@
 package com.example.supuni.mybusapplication;
 
+import android.app.LoaderManager;
 import android.app.Service;
 import android.bluetooth.BluetoothClass;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
@@ -27,6 +29,35 @@ public class GPSTracker extends Service implements LocationListener{
     Location location;
     double latitude;
     double longitude;
+
+    private static final long MIN_DISTANCE_FOR_UPDATES =10;
+    private static final long MIN_TIME_BW_UPDATES =1000 * 60 * 1;
+    protected LocationManager locationManager;
+
+    public GPSTracker (Context context){
+        this.mContext =context;
+        getLocation();
+    }
+
+    public void getLocation(){
+        try {
+            locationManager =(LocationManager) mContext.getSystemService(LOCATION_SERVICE);
+
+            isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+            isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            if(!isGPSEnabled && !isNetworkEnabled){
+
+            }
+            else {
+                this.canGetLocation=true;
+
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 
     @Nullable
     @Override
