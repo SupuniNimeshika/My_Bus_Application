dca54c8863ba8ca61ddd790edd6007eead3b3ac1
Your Name <supuni.nimeshika@gmail.com>
2018-04-12 20:08:54 +0530

    MainActivity Link With GPSTracker

--- app/src/main/java/com/example/supuni/mybusapplication/MainActivity.java
+++ app/src/main/java/com/example/supuni/mybusapplication/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.supuni.mybusapplication;
 
 import android.Manifest;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.test.mock.MockPackageManager;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -19,5 +22,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            if(ActivityCompat.checkSelfPermission(this,mPermission) != MockPackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(this,new String[]{mPermission},REQUEST_CODE_PERMISSION);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        btnShowLocation =(Button) findViewById(R.id.button);
+        btnShowLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gps =new GPSTracker(MainActivity.this);
+                location =(TextView) findViewById(R.id.locationText);
+                if(gps.canGetLocation()){
+                    double latitude =gps.getLatitude();
+                    double longitude =gps.getLongitude();
+                    location.setText(latitude+""+longitude);
+                }else {
+                    gps.showSettingAlert();
+                }
+            }
+        });
     }
 }
