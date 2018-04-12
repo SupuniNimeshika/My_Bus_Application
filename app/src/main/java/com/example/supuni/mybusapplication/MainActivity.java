df1e002051c9384d408bb16ff59f4796a8e225d1
Your Name <supuni.nimeshika@gmail.com>
2018-04-12 19:18:24 +0530

    Add Some Variable Compatible With Button

--- app/src/main/java/com/example/supuni/mybusapplication/MainActivity.java
+++ app/src/main/java/com/example/supuni/mybusapplication/MainActivity.java
@@ -1,9 +1,19 @@
 package com.example.supuni.mybusapplication;
 
+import android.Manifest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnShowLocation;
+    private static final int REQUEST_CODE_PERMISSION =2;
+    String mPermission = Manifest.permission.ACCESS_FINE_LOCATION;
+
+    GPSTracker gps;
+    TextView location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
